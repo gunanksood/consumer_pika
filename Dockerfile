a5fac5014c0b0d7d124c89b7bf70749f10a71fad
gunanksood <gunanksood@gmail.com>
2018-09-06 19:53:08 +0530

    Trying to deploy on docker

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM ubuntu:latest
+RUN apt-get update -y
+RUN apt-get install -y python-pip python-dev build-essential
+COPY . /app1
+WORKDIR /app1
+RUN pip install -r requirements.txt
+ENTRYPOINT ["python"]
+CMD ["workassign.py"]
--- workerassign.py
+++ workerassign.py
@@ -1,25 +1,26 @@
-#!/usr/bin/env python
 import pika
 import time
 import pymongo
 
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+cred = pika.PlainCredentials('gunank', 'gunank')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.17.0.1',
+                                                                           port=5673, virtual_host='/',
+                                                                           credentials=cred))
 channel = connection.channel()
 
 channel.queue_declare(queue='tasks_queue')
 print(' [*] Waiting for messages. To exit press CTRL+C')
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydatabase"]
-mycol = mydb["customers"]
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["mydatabase"]
+# mycol = mydb["customers"]
 
 
 def callback(ch, method, properties, body):
     l = body.split(",")
-    mydata = {"ID": l[0], "name": l[1], "Marks": l[2]}
-    x = mycol.insert_many(mydata)
-    print(" [x] Received %s hello " %l[0])
-    time.sleep(body.count(b'.'))
+    # mydata = {"ID": l[0], "name": l[1], "Marks": l[2]}
+    # x = mycol.insert_one(mydata)
+    print(" [x] Received %s hello " % l)
+    # time.sleep(body.count(b'.'))
     print(" [x] Done")
     # ch.basic_ack(delivery_tag=method.delivery_tag)
 
